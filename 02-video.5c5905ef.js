14b7de5f78f6d34ecb662d5a77e0a93025e1be87
A-ZuMa <84996280+A-ZuMa@users.noreply.github.com>
2023-08-13 19:35:25 +0000

    Deploying to gh-pages from @ A-ZuMa/goit-js-hw-08@d303dac2cd3a15c15544935f353b4db355768f71 🚀

---
+++ 02-video.5c5905ef.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("iframe");new Player(e)}();
+//# sourceMappingURL=02-video.5c5905ef.js.map
---
+++ 02-video.c4b22035.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("iframe");new Player(e);
+//# sourceMappingURL=02-video.c4b22035.js.map
